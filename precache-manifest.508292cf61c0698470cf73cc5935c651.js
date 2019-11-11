bcf92364a78e2ad2b6481de565120c8e4c23a2f5
Chris Vickery <chrisinajar@gmail.com>
2019-11-11 15:15:42 -0800

    Updates

---
+++ precache-manifest.508292cf61c0698470cf73cc5935c651.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a17a16429d92185926c01a9b7889a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5235b25a3ca78b4bdc58",
+    "url": "/static/js/2.bd1e2e97.chunk.js"
+  },
+  {
+    "revision": "f16ae68bdaaaffa03b46",
+    "url": "/static/js/main.b549fe85.chunk.js"
+  },
+  {
+    "revision": "73571925f40e2f0b153f",
+    "url": "/static/js/runtime-main.95786b86.js"
+  }
+]);
